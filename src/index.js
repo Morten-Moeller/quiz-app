43a0d8824033eeac2ce119bea1a6bbb95c922590
Morten Möller <git@mortenm.de>
2021-04-27 16:21:24 +0200

    Add navigation with JS

--- src/index.js
+++ src/index.js
@@ -9,3 +9,63 @@ buttonDarkmode.addEventListener('click', () => {
     ? (buttonDarkmode.innerHTML = '🌞')
     : (buttonDarkmode.innerHTML = '🌚')
 })
+
+// Navigation
+
+const buttonHome = document.querySelector('.button-home')
+const buttonBookmark = document.querySelector('.button-bookmark')
+const buttonForm = document.querySelector('.button-form')
+const buttonProfile = document.querySelector('.button-profile')
+
+const mainHome = document.querySelector('.home-page')
+const mainBookmark = document.querySelector('.bookmark-page')
+const mainForm = document.querySelector('.form-page')
+const mainProfile = document.querySelector('.profile-page')
+
+buttonHome.addEventListener('click', () => {
+  mainHome.classList.remove('hidden')
+  mainBookmark.classList.add('hidden')
+  mainForm.classList.add('hidden')
+  mainProfile.classList.add('hidden')
+
+  buttonHome.classList.add('app-footer__svg--active')
+  buttonBookmark.classList.remove('app-footer__svg--active')
+  buttonForm.classList.remove('app-footer__svg--active')
+  buttonProfile.classList.remove('app-footer__svg--active')
+})
+
+buttonBookmark.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  mainBookmark.classList.remove('hidden')
+  mainForm.classList.add('hidden')
+  mainProfile.classList.add('hidden')
+
+  buttonHome.classList.remove('app-footer__svg--active')
+  buttonBookmark.classList.add('app-footer__svg--active')
+  buttonForm.classList.remove('app-footer__svg--active')
+  buttonProfile.classList.remove('app-footer__svg--active')
+})
+
+buttonForm.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  mainBookmark.classList.add('hidden')
+  mainForm.classList.remove('hidden')
+  mainProfile.classList.add('hidden')
+
+  buttonHome.classList.remove('app-footer__svg--active')
+  buttonBookmark.classList.remove('app-footer__svg--active')
+  buttonForm.classList.add('app-footer__svg--active')
+  buttonProfile.classList.remove('app-footer__svg--active')
+})
+
+buttonProfile.addEventListener('click', () => {
+  mainHome.classList.add('hidden')
+  mainBookmark.classList.add('hidden')
+  mainForm.classList.add('hidden')
+  mainProfile.classList.remove('hidden')
+
+  buttonHome.classList.remove('app-footer__svg--active')
+  buttonBookmark.classList.remove('app-footer__svg--active')
+  buttonForm.classList.remove('app-footer__svg--active')
+  buttonProfile.classList.add('app-footer__svg--active')
+})
